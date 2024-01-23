22a71ccb0f547b91ae23dc46d3c80f2edffe9b11
Marki <134964583+marktudor11@users.noreply.github.com>
2024-01-23 15:15:42 -0500

    Update HandleInput.java

--- InputHandling/HandleInput.java
+++ InputHandling/HandleInput.java
@@ -12,29 +12,6 @@ public class HandleInput {
     
     //New Function to add ActionListener to buttons that are currently set up
 
-
-
-private void addButtonListener(){
-
-    //use nested loop to iterate over each button in chessboard
-    for(int y = 0; y < 8; y++){
-        for(int x = 0; x < 8; x++){
-
-            //obtains the Jbutton at current position (x,y)
-            //add ActionListener to each button
-            JButton button = squares[x][y];
-            button.addActionListener(new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e){
-                    
-                    //pass coordinates in call to this method
-                    handleButtonPress(x, y);
-                }
-            });
-        }
-    }
-}
-
 //New Function to handle button press events
 
 private void handleButtonPress(int x, int y){
