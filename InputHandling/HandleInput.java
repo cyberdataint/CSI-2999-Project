215d234d0d8765a15e16bf89579822bcb163b76e
Marki <134964583+marktudor11@users.noreply.github.com>
2024-01-22 20:26:20 -0500

    Create HandleInput.java
    Eliminated ActionListener creation in InputHandling.

---
+++ InputHandling/HandleInput.java
@@ -0,0 +1,30 @@
+package InputHandling;
+
+import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+import mechanics.GameBoard;
+
+//import visuals.user_interface.Board;
+
+public class HandleInput {
+    
+    public static void handleButtonPress(int x, int y){
+
+        //add logic for handling button press event
+        System.out.printf("Button Handled");
+
+
+
+
+
+
+
+
+    }
+
+
+}
