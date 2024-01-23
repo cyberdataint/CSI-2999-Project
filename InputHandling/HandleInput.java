964874c0080abe4e718d47f8e1d39913b0dddadd
Marki <134964583+marktudor11@users.noreply.github.com>
2024-01-23 15:13:22 -0500

    Update HandleInput.java

--- InputHandling/HandleInput.java
+++ InputHandling/HandleInput.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 
 import mechanics.GameBoard;
 
+import mechanics.Space;
+
 //import visuals.user_interface.Board;
 
 public class HandleInput {
@@ -16,6 +18,22 @@ public class HandleInput {
 
         //add logic for handling button press event
         System.out.printf("Button Handled");
+
+        /* 1.  Retrieve Piece that is clicked on/ Know which piece was clicked
+
+
+        2. Detect Danger
+
+
+        3. View valid moves
+
+
+
+        4. Move piece to desired valid location
+         * 
+         * 
+         * 
+         */
 
 
 
